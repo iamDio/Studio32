049d49b25b7f20bae48cdf07abb122957fa93d24
iamDio <claudio.villani00@gmail.com>
2017-10-10 23:15:32 -0400

    working on dynamically generating playlist items

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,7 @@
-console.log('im listening')
+document.getElementById('requestItem').addEventListener('keypress', function(e){
+	var key = e.which || e.keyCode;
+	if (key === 13) {
+		let inputField = document.createElement('OL', 'INPUT')
+		document.getElementById('request').appendChild(inputField)
+	}
+})
