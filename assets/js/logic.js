cc9f2fcb08efd21c6919c84e48b4d63984e17fe8
iamDio <claudio.villani00@gmail.com>
2017-10-12 23:43:59 -0400

    fixed bug, new bugs arrived

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,19 @@
-document.getElementById('requestItem').addEventListener('keypress', function(e){
-	var key = e.which || e.keyCode;
-	if (key === 13) {
-		let inputField = document.createElement('OL', 'INPUT')
-		document.getElementById('request').appendChild(inputField)
-	}
-})
+
+
+function createRequest(){
+	let classname = document.getElementsByClassName('requestItem');
+		Array.from(classname).forEach(function(element){
+			document.body.addEventListener('keypress', function(e){
+				let key = e.which || e.keyCode;
+				if (key === 13) {
+					let inputField = document.createElement('OL')
+					inputField.innerHTML = "<input class='requestItem' type='text' name='Requests' placeholder='enter song name'></input>"
+					
+					document.getElementById('request').appendChild(inputField)
+				}
+
+			})
+		})
+}
+
+createRequest();
