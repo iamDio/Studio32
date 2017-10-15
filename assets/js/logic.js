f91271ed63c4f95b83d10ad85b42718bde7e9837
iamDio <claudio.villani00@gmail.com>
2017-10-14 22:59:29 -0400

    added working buttons which add new songs to their respective lists

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,19 +1,26 @@
 
-
-function createRequest(){
-	let classname = document.getElementsByClassName('requestItem');
-		Array.from(classname).forEach(function(element){
-			document.body.addEventListener('keypress', function(e){
-				let key = e.which || e.keyCode;
-				if (key === 13) {
-					let inputField = document.createElement('OL')
-					inputField.innerHTML = "<input class='requestItem' type='text' name='Requests' placeholder='enter song name'></input>"
-					
-					document.getElementById('request').appendChild(inputField)
-				}
-
+function createRequest() {
+	const requestItem = document.getElementsByClassName('requestItem');
+			Array.from(requestItem).forEach(function(element){
+				element.addEventListener('click', function(e){
+							console.log(e)
+						let inputField = document.createElement('OL')
+						inputField.innerHTML =  "<input type='text' name='Requests' placeholder='enter song name'></input>"
+	                    document.getElementById('request').appendChild(inputField)
+				})
 			})
-		})
 }
-
 createRequest();
+
+function createDnp() {
+	const createDnp = document.getElementsByClassName('createDnp');
+			Array.from(createDnp).forEach(function(e){
+				e.addEventListener('click', function(e){
+							console.log(e)
+						let dnpField = document.createElement('OL')
+						dnpField.innerHTML =  "<input type='text' name='doNotPlay' placeholder='enter song name'></input>"
+	                    document.getElementById('doNotPlay').appendChild(dnpField)
+				})
+			})
+}
+createDnp();
