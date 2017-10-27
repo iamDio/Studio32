a504c5b3d32e5de65afa84339a3c038840acf8de
iamDio <claudio.villani00@gmail.com>
2017-10-26 23:15:41 -0400

    connected info page to router

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const contactForm = require('../models/contactForm.js');
 
 const router = express.Router();
-
+/*
 router.get('/', function(req, res){
-	res.render('index')
+	res.sendFile(path.join(__dirname, '../views/index.html'))
+});*/
+
+router.get('/info', function(req,res){
+	res.sendFile(path.join(__dirname, '../views/info.html'));
 });
 
 router.post('/contact/submit',function(req, res){
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-
 const contactForm = require('./models/contactForm.js');
 const routes = require('./routes/routes');
 
